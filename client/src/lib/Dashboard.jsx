1ac25b889d484c90963b7e662ea7dac7d6e33281
Shaurya Kumar <kshau@Shauryas-Air.attlocal.net>
2024-03-08 21:03:23 -0500

    menu

--- client/src/lib/Dashboard.jsx
+++ client/src/lib/Dashboard.jsx
@@ -1,70 +1,11 @@
-import './style.css';
-
-import dashboardIcon from '../assets/icon/dashboard.svg';
-import assignmentsIcon from '../assets/icon/assignments.svg';
-import timersIcon from '../assets/icon/timers.svg';
-import forumsIcon from '../assets/icon/forums.svg';
-import calculateGradeIcon from '../assets/icon/calculate-grade.svg';
-
-import logo from "../assets/logo.svg"
+import Menu from './Menu'
 
 function Dashboard() {
-
     return (
         <>
-            <div className="topBar">
-
-                <div className="logo">
-                    <img src={logo}/>
-                    <span>StudyNest</span>
-                </div>
-
-                <div className="pageTitle">
-                    <span>Dashboard</span>
-                    <img src={dashboardIcon}/>
-                </div>
-
-            </div>
-
-            <div className="sideBar">
-            
-                <a href="/dashboard" className="sideBarActive">
-                    <img src={dashboardIcon}/>
-                    <span>Dashboard</span>
-                </a>
-
-                <div className="sideBarItemDiv"></div>
-
-                <a href="/assignments">
-                    <img src={assignmentsIcon}/>
-                    <span>Assignments</span>
-                </a>
-
-                <div className="sideBarItemDiv"></div>
-
-                <a href="/timers">
-                    <img src={timersIcon}/>
-                    <span>Timers</span>
-                </a>
-
-                <div className="sideBarItemDiv"></div>
-
-                <a href="/forums">
-                    <img src={forumsIcon}/>
-                    <span>Forums</span>
-                </a>
-
-                <div className="sideBarItemDiv"></div>
-
-                <a href="/calculate-grade">
-                    <img src={calculateGradeIcon}/>
-                    <span>Calculate Grade</span>
-                </a>
-
-            </div>
+            <Menu page="Dashboard"></Menu>
         </>
     )
-
 }
 
 export default Dashboard;
